12eef9ae22a4a17562e38dae720ad9fadaee1a66
Francislainy <francislainy.campos@gmail.com>
2024-01-05 16:21:11 +0000

    Load banner based on start game button press

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
 import './App.css';
 import Card from './Card';
 import Banner from './Banner';
+import { useState } from 'react';
 
 function App() {
+  const [hasGameStarted, setHasGameStarted] = useState(false)
+  const [bannerTitle, setBannerTitle] = useState('Chinese Character Test')
+  const [bannerDescriotion, setBannerDescription] = useState('Test your knowledge of Chinese characters in this fun and interactive game. Click the button below to start the game.')
+  const [bannerButtonText, setBannerButtonText] = useState('Start Game')
+
+
+  const handleStartGame = () => {
+    setHasGameStarted(true)
+    setBannerDescription("Click on the characters you know. Once you're done, click the button below to see your total.")
+    setBannerButtonText("See Results")
+  }
+
   return (
     <div className="app">
       <Banner
-        title={'Chinese Character Test'}
-        description={'Test your knowledge of Chinese characters in this fun and interactive game. Click the button below to start the game.'}
-        buttonText={'Start Game'}
+        title={bannerTitle}
+        description={bannerDescriotion}
+        buttonText={bannerButtonText} handleStartGame={handleStartGame}
       />
-      <Card />
+      {hasGameStarted && <Card />}
     </div>
   );
 }
