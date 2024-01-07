064180ea5165b41b15c9e66c2818cdcf57893c77
Francislainy <francislainy.campos@gmail.com>
2024-01-07 11:23:51 +0000

    Add click for selected card

--- src/Card.js
+++ src/Card.js
@@ -1,23 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Card.css';
 
-function card() {
+function Card() {
+  const [selectedCardIds, setSelectedCardIds] = useState([]);
+
+  const handleClick = (cardId) => {
+    setSelectedCardIds((prevSelectedCardIds) => {
+      if (prevSelectedCardIds.includes(cardId)) {
+        return prevSelectedCardIds.filter((id) => id !== cardId); // Unselect the card if it's already selected
+      } else {
+        return [...prevSelectedCardIds, cardId]; // Select the card if it's not selected
+      }
+    });
+  };
+
+  const cardList = [
+    { id: 1, chineseCharacter: '中' },
+    { id: 2, chineseCharacter: '国' },
+    { id: 3, chineseCharacter: '字' },
+    { id: 4, chineseCharacter: '测' },
+  ];
+
   return (
     <div className="card__row">
-        <div className="card">
-          <span className="card__chinese-character">中</span>
+      {cardList.map((card) => (
+        <div
+          className={`card ${selectedCardIds.includes(card.id) ? 'card--clicked' : ''}`}
+          onClick={() => handleClick(card.id)}
+          key={card.id}
+        >
+          <span className="card__chinese-character">{card.chineseCharacter}</span>
         </div>
-        <div className="card">
-          <span className="card__chinese-character">国</span>
-        </div>
-        <div className="card">
-          <span className="card__chinese-character">字</span>
-        </div>
-        <div className="card">
-          <span className="card__chinese-character">测</span>
-        </div>
-      </div>
-  )
+      ))}
+    </div>
+  );
 }
 
-export default card;
+export default Card;
