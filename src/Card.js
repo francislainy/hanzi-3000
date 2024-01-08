5477216eea5524fa8962e9d6a43184752e7cc585
Francislainy <francislainy.campos@gmail.com>
2024-01-08 12:19:55 +0000

    Retrieve character definition from api

--- src/Card.js
+++ src/Card.js
@@ -4,6 +4,8 @@ import './Card.css';
 function Card({ cardList, selectedCardIds, setSelectedCardIds }) {
   const [hoveredCard, setHoveredCard] = useState(null);
 
+
+
   const handleClick = (cardId) => {
     setSelectedCardIds((prevSelectedCardIds) => {
       if (prevSelectedCardIds.includes(cardId)) {
@@ -14,9 +16,27 @@ function Card({ cardList, selectedCardIds, setSelectedCardIds }) {
     });
   };
 
-  const handleDefClick = (cardId) => {
-    alert(`Definition of card ${cardId}`);
+  const handleDefClick = (chineseCharacter) => {
+    const url = new URL('https://chinese-english-dictionary-api.p.rapidapi.com/definition');
+    url.search = new URLSearchParams({ simplified: chineseCharacter, exact: true });
+  
+    fetch(url, {
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'X-RapidAPI-Key': 'dca7d0520dmshbffb5c1b0b6b9e1p1a0763jsn1204add21968',
+        'X-RapidAPI-Host': 'chinese-english-dictionary-api.p.rapidapi.com'
+      }
+    })
+    .then(response => response.json())
+    .then(data => {
+      const definitions = data.entries.flatMap(entry => entry.definitions);
+      alert(`Definition of card ${chineseCharacter}: ${definitions.join(', ')}`);
+    })
+    .catch(error => console.error('Error:', error));
   };
+
+
 
   return (
     <div className="card__row">
@@ -30,7 +50,7 @@ function Card({ cardList, selectedCardIds, setSelectedCardIds }) {
         >
           <span className="card__chinese-character">{card.chineseCharacter}</span>
           {hoveredCard === card.id && (
-            <button className="def-button" onClick={(e) => { e.stopPropagation(); handleDefClick(card.id); }}>Def.</button>
+            <button className="def-button" onClick={(e) => { e.stopPropagation(); handleDefClick(card.chineseCharacter); }}>Def.</button>
           )}
         </div>
       ))}
